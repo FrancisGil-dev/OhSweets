4dc9e7cfc3ed5d27b9e782ba2570fe8f9d8043e1
Francis.Dev <francisgil.dev@gmail.com>
2023-11-22 23:39:19 +0800

    commit

--- script.js
+++ script.js
@@ -3,24 +3,27 @@
 //function to scroll to view
 const scrollView = () => {
     //get the doms
-    const navs = document.querySelector('li');
-    const section = document.querySelector('section');
+    const navs = document.querySelectorAll('li');
+    const section = document.querySelectorAll('section');
    //navs
-   const home = navs[0];
-   const about = navs[1];
-   const menu = navs[2];
-   const contact = navs[3];
 
    //function to click to scroll
    const scroll = (sec) => {sec.scrollIntoView({behavior: 'smooth'})}
 
    //addevent Listener
-   about.addEventListener('click' () => {
-    scroll(section[1])
-   })
-
+    navs[0].addEventListener('click', () => scroll(section[0]))
+    navs[1].addEventListener('click', () => scroll( section[1]))
+    navs[2].addEventListener('click', () => scroll(section[2]))
+    navs[3].addEventListener('click', () => scroll(section[3]))
 }
 scrollView()
 
+//toggle hamburger icon
+const toggleList = () => {
+    const hamburger = document.getElementById('hamburger');
+    const list = document.getElementById('toggleList')
+    hamburger.addEventListener('click', () => list.classList.toggle('toggled'))
+}
+toggleList()
 
 
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      height: {
+        '128': '32rem',
+      },
+      fontFamily: {
+        custom: ['Poppins', 'sans-serif'],
+       fontFamily: ['Satisfy', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
